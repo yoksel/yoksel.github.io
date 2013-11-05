00d3c964182835b4c8d1017ba575291ed7c398c5
yoksel <yoksel@yoksel.local>
2013-11-05 23:43:03 +0400

    Good grunt for style optimisation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,3 @@
-/*!
- * FireShell Gruntfile
- * http://getfireshell.com
- * @author Todd Motto
- */
-
 'use strict';
 
 /**
@@ -11,27 +5,19 @@
  */
 module.exports = function (grunt) {
 
-  /**
-   * Dynamically load npm tasks
-   */
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-
-  /**
-   * FireShell Grunt config
-   */
   grunt.initConfig({
 
     pkg: grunt.file.readJSON('package.json'),
 
     /**
-     * Set project info
+     * Project info
      */
     project: {
       css_src: [
-        'src/css/'
+        'src/css'
       ],
       css_res: [
-        'assets/css/'
+        'assets/css'
       ],
       js: [
         'src/js/*.js'
@@ -40,29 +26,24 @@ module.exports = function (grunt) {
 
     /**
      * Project banner
-     * Dynamically appended to CSS/JS files
      * Inherits text from package.json
      */
     tag: {
       banner: '/*!\n' +
-              ' * <%= pkg.name %>\n' +
-              ' * <%= pkg.title %>\n' +
-              ' * <%= pkg.url %>\n' +
-              ' * @author <%= pkg.author %>\n' +
-              ' * @version <%= pkg.version %>\n' +
+              ' * Name: <%= pkg.name %>\n' +
+              ' * Project: <%= pkg.description %>\n' +
+              ' * Author: <%= pkg.author %>\n' +
+              ' * Version: <%= pkg.version %>\n' +
               ' */\n'
     },
 
     /**
-     * Compile Sass/SCSS files
-     * https://github.com/gruntjs/grunt-contrib-sass
-     * Compiles all Sass/SCSS files and appends project banner
+     * https://npmjs.org/package/grunt-contrib-sass
      */
     sass: {
       dev: {
         options: {
-          style: 'expanded',
-          banner: '<%= tag.banner %>'
+          style: 'expanded'
         },
         files: {
           '<%= project.css_src %>/style.unprefixed.css': '<%= project.css_src %>/style.scss'
@@ -70,59 +51,74 @@ module.exports = function (grunt) {
       },
       dist: {
         options: {
-          style: 'expanded'
+          style: 'expanded',
+          banner: '<%= tag.banner %>'
         },
         files: {
           '<%= project.css_src %>/style.unprefixed.css': '<%= project.css_src %>/style.scss'
         }
       }
     },
-    autoprefixer: {
-      single_file: {
-      src: '<%= project.css_src %>/style.unprefixed.css',
-      dest: '<%= project.css_src %>/style.prefixed.css'
-      },
-    },
 
-    csscomb: {
-      options: {
-        // sortOrder: '/path/to/custom-sort-order.json'
+    /**
+     * https://npmjs.org/package/grunt-autoprefixer
+     */
+    autoprefixer: {
+      dev: {
+        options: {},
+        src: '<%= project.css_src %>/style.unprefixed.css',
+        dest: '<%= project.css_res %>/style.css'
       },
-      files: {
-          '<%= project.css_src %>/style_resort.css': ['<%= project.css_src %>/style.prefixed.css'],
-        },
+      dist: {
+        options: {},
+        src: '<%= project.css_src %>/style.unprefixed.css',
+        dest: '<%= project.css_src %>/style.prefixed.css'
+      },
     },
 
     /**
-     * Runs tasks against changed watched files
-     * https://github.com/gruntjs/grunt-contrib-watch
-     * Watching development files and run concat/compile tasks
-     * Livereload the browser once complete
+     * https://npmjs.org/package/grunt-csso
+     */
+    csso: {
+      compress: {
+        options: {
+          report: 'min'
+        },
+        files: {
+          '<%= project.css_res %>/style.css': ['<%= project.css_src %>/style.prefixed.css']
+        }
+      }
+    },
+
+    /**
+     * https://npmjs.org/package/grunt-contrib-watch
      */
     watch: {
       sass: {
         files: '<%= project.css_src %>{,*/}*.{scss,sass}',
-        tasks: ['sass:dev', 'autoprefixer', 'csscomb']
+        tasks: ['sass:dev', 'autoprefixer']
       }
     }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-csso');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   /**
    * Default task
    * Run `grunt` on the command line
    */
   grunt.registerTask('default', [
-    'sass:dev',
     'watch'
-  ]);
+    ]);
 
-  /**
-   * Build task
-   * Run `grunt build` on the command line
-   * Then compress all JS/CSS files
-   */
   grunt.registerTask('build', [
-    'sass:dist'
-  ]);
+    'sass:dist',
+    'autoprefixer:dist',
+    'csso'
+    ]);
+
 
 };
