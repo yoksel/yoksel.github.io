d11e9acdce7a9f8801b58bf33e9b6b4c2a3d7804
yoksel <yokselzok@gmail.com>
2020-07-21 23:34:54 +0300

    Updates

--- assets/js/common.js
+++ assets/js/common.js
@@ -5,6 +5,51 @@ document.documentElement.classList.remove('no-js');
 const markerHandler = new MarkerHandler();
 markerHandler.addEvents();
 
+// Tags
+initTagsList();
+
+function initTagsList () {
+  const tagsList = document.querySelector('.tags-list');
+
+  if (!tagsList) {
+    return;
+  }
+
+  const posts = document.querySelectorAll('.post-item');
+  let currentTag = document && document.location.hash
+    ? document.location.hash.substring(1)
+    : '';
+
+  tagsList.addEventListener('click', (event) => {
+    const button = event.target.closest('button[data-target-tag]');
+
+    if (!button) {
+      return;
+    }
+
+    const buttonTag = button.dataset.targetTag;
+
+    if (buttonTag === currentTag) {
+      currentTag = null;
+    } else {
+      currentTag = buttonTag;
+    }
+
+    posts.forEach(post => {
+      if (currentTag) {
+        if (post.classList.contains(`tag-${currentTag}`)) {
+          post.classList.remove('hidden');
+        } else {
+          post.classList.add('hidden');
+        }
+      } else {
+        post.classList.remove('hidden');
+      }
+    });
+  });
+}
+
+// Comments
 const commentsContainer = document.querySelector('.post__comments');
 const loadCommentsButton = document.querySelector('.post__load-comments');
 var disqusShortname = 'css-yoksel';
@@ -12,9 +57,8 @@ var disqusShortname = 'css-yoksel';
 const disqusCommentsStr = `<div class="widget widget--disqus" id="comments">
   <div id="disqus_thread"></div>
   <noscript>Включите JavaScript <a href="http://disqus.com/?ref_noscript">чтобы увидеть комментарии.</a></noscript>
+  <div class="dsq-brlink">Загрузка...</div>
 </div>`;
-
-/* <a class="dsq-brlink">Комменты пытаются загрузиться. Если не у них не получилось, попробуйте обновить страницу.</a> */
 
 if (commentsContainer && loadCommentsButton) {
   loadCommentsButton.addEventListener('click', () => {
