94a4f749c94550333afc127e20ddc69473f9970a
yoksel <yokselzok@gmail.com>
2020-07-23 16:41:03 +0300

    Updates

--- assets/js/common.js
+++ assets/js/common.js
@@ -27,6 +27,7 @@ function initTagsList () {
     currentTagButton = document.querySelector(`button[data-target-tag="${currentTag}"]`);
     currentTagButton.classList.add(currentTagClass);
     postsListTitle.innerHTML = `Статьи по тегу «${currentTagButton.innerHTML}»`;
+    filterPostsByTags();
   } else {
     postsListTitle.innerHTML = 'Все статьи';
   }
@@ -57,6 +58,10 @@ function initTagsList () {
       postsListTitle.innerHTML = `Статьи по тегу «${currentTagButton.innerHTML}»`;
     }
 
+    filterPostsByTags();
+  });
+
+  function filterPostsByTags () {
     posts.forEach(post => {
       if (currentTag) {
         if (post.classList.contains(`tag-${currentTag}`)) {
@@ -68,7 +73,7 @@ function initTagsList () {
         post.classList.remove('hidden');
       }
     });
-  });
+  }
 }
 
 // Comments
