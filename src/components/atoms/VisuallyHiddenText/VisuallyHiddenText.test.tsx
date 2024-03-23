0c32ffaf8e66664263ed21312a8e9b8d5342c4e3
yoksel <juliahtml@gmail.com>
2024-03-23 17:57:06 +0000

    Add tag to VisuallyHiddenText; improve tests

--- src/components/atoms/VisuallyHiddenText/VisuallyHiddenText.test.tsx
+++ src/components/atoms/VisuallyHiddenText/VisuallyHiddenText.test.tsx
@@ -4,12 +4,19 @@ import '@testing-library/jest-dom';
 import VisuallyHiddenText from '.';
 
 describe('VisuallyHiddenText', () => {
-  test('to be link if href is not equal to slug', () => {
+  test('to be rendered', () => {
     const text = 'hello';
 
     const { getByText } = render(<VisuallyHiddenText>{text}</VisuallyHiddenText>);
 
-    const link = getByText(text);
     expect(getByText(text)).toBeInTheDocument();
+  });
+
+  test('to be heading if element provided', () => {
+    const text = 'hello';
+
+    const { getByRole } = render(<VisuallyHiddenText element="h3">{text}</VisuallyHiddenText>);
+
+    expect(getByRole('heading', { name: text })).toBeInTheDocument();
   });
 });
