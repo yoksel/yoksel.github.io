ce5a1ff7f847340f6d3ed9e370740755f7c3c0e5
yoksel <juliahtml@gmail.com>
2024-03-26 10:10:39 +0000

    Move static files to public

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,11 +1,9 @@
 import { initMarker } from './marker-handler.js';
 import { initTagsList } from './init-tags-list.js';
-import { initComments } from './init-comments.js';
 import { initPhotoWidget } from './init-photo-widget.js';
 
 document.documentElement.classList.remove('no-js');
 
 initMarker();
 initTagsList();
-initComments();
 initPhotoWidget();
--- static/README.md
+++
@@ -1,5 +0,0 @@
-# Про CSS
-
-Статическое содержимое сайта http://css.yoksel.ru/
-
-Сгенерировано с использованием [Gatsby](https://www.gatsbyjs.org/)
