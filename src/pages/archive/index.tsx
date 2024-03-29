68f4b301231a31f57c451dc288208edea1887248
yoksel <juliahtml@gmail.com>
2024-03-29 22:06:09 +0000

    Move pages

--- src/pages/archive/index.tsx
+++ src/pages/archive/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import type { InferGetStaticPropsType, GetStaticProps } from 'next';
 
-import { getAllArticles } from '../utils/api';
-import Layout from '../components/molecules/Layout';
-import ArticlesCardsList from '../components/molecules/ArticlesCardsList';
-import { ArticleWithDate } from '../components/molecules/ArticleCard';
+import { getAllArticles } from '../../utils/api';
+import Layout from '../../components/molecules/Layout';
+import ArticlesCardsList from '../../components/molecules/ArticlesCardsList';
+import { ArticleWithDate } from '../../components/molecules/ArticleCard';
 
 export const getStaticProps = (async () => {
   const posts = (await getAllArticles(
