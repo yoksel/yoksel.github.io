61488d221dae8c44a08b2379b76af5edfd2e2254
yoksel <juliahtml@gmail.com>
2024-03-29 22:21:04 +0000

    Tiny tweaks

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  basePath: '/',
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
