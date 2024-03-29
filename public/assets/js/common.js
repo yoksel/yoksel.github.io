c86ad1d1c087592ced57207b9059042bfa82a141
yoksel <juliahtml@gmail.com>
2024-03-29 12:51:48 +0000

    Remove needless files

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,9 +1,7 @@
 import { initMarker } from './marker-handler.js';
-import { initTagsList } from './init-tags-list.js';
 import { initPhotoWidget } from './init-photo-widget.js';
 
 document.documentElement.classList.remove('no-js');
 
 initMarker();
-initTagsList();
 initPhotoWidget();
