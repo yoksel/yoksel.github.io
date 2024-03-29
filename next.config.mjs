85b3ff7956afa25bcac2ce6df0288486ab3d6df1
yoksel <juliahtml@gmail.com>
2024-03-29 21:29:58 +0000

    Try to fix deploy

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'images.unsplash.com',
-  //       port: '',
-  //       pathname: '/**',
-  //     },
-  //   ],
-  // },
 };
 
 export default nextConfig;
