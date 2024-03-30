d94926d3f3aaa0df7c862e61006af6e125ed624c
yoksel <juliahtml@gmail.com>
2024-03-30 10:55:12 +0000

    Randomize logo effects

--- src/components/molecules/Logo/index.tsx
+++ src/components/molecules/Logo/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import Link from '../../atoms/Link';
 
@@ -9,44 +9,100 @@ interface LogoProps {
   parent?: string;
 }
 
-const Logo = ({ isMain, parent = 'header' }: LogoProps) => {
-  const containerClassName = `${parent}-logo`;
-  const itemClassName = `${containerClassName}__item`;
-  const contentClassName = `${containerClassName}__content`;
-  const containerStyles = styles[containerClassName];
+interface LogoContentProps {
+  itemClassName: string;
+  isInHeader: boolean;
+}
+const LogoContent = ({ isInHeader, itemClassName }: LogoContentProps) => {
+  if (!isInHeader) {
+    return 'Про CSS';
+  }
+
   const itemStyles = styles[itemClassName];
   const itemAboutStyles = styles[`${itemClassName}--about`];
   const itemCSSStyles = styles[`${itemClassName}--css`];
-  const contentStyles = styles[contentClassName];
-  const linkStyles = styles[`${contentClassName}--link`];
-  const notLinkStyles = styles[`${contentClassName}__not-link`];
 
-  const logoContent = (
+  return (
     <>
       <span className={classNames(itemStyles, itemAboutStyles)}>Про</span>&nbsp;
       <span className={classNames(itemStyles, itemCSSStyles)}>CSS</span>
     </>
   );
+};
+
+interface LogoContainerProps extends React.PropsWithChildren {
+  containerClassName: string;
+  theme?: string;
+}
+
+const LogoContainer = ({ containerClassName, theme, children }: LogoContainerProps) => {
+  return (
+    <div
+      className={classNames(
+        styles[containerClassName],
+        theme && styles[`${containerClassName}--${theme}`],
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+const Logo = ({ isMain, parent = 'header' }: LogoProps) => {
+  const containerClassName = `${parent}-logo`;
+  const itemClassName = `${containerClassName}__item`;
+  const contentClassName = `${containerClassName}__content`;
+  const linkClassName = `${containerClassName}__link`;
+  const isInHeader = parent === 'header';
+  const themes = ['circle', 'rays'];
+  const [theme, setTheme] = useState<string>();
+
+  useEffect(() => {
+    if (!isInHeader) return;
+
+    const randomIndex = Math.floor(Math.random() * themes.length);
+    if (themes[randomIndex]) {
+      setTheme(themes[randomIndex]);
+    }
+  }, []);
 
   if (isMain) {
     return (
-      <div className={containerStyles}>
-        <span className={classNames(contentStyles, notLinkStyles)}>{logoContent}</span>
-      </div>
+      <LogoContainer
+        containerClassName={containerClassName}
+        theme={theme}
+      >
+        <span className={classNames(styles[contentClassName])}>
+          <LogoContent
+            itemClassName={itemClassName}
+            isInHeader={isInHeader}
+          />
+        </span>
+      </LogoContainer>
     );
   }
 
   return (
-    <div className={containerStyles}>
+    <LogoContainer
+      containerClassName={containerClassName}
+      theme={theme}
+    >
       <Link
-        className={classNames(contentStyles, linkStyles)}
+        className={classNames(
+          styles[contentClassName],
+          styles[linkClassName],
+          theme && styles[`${linkClassName}--${theme}`],
+        )}
         href="/"
         dataName="Про CSS"
         ariaLabel="Про CSS"
       >
-        {logoContent}
+        <LogoContent
+          itemClassName={itemClassName}
+          isInHeader={parent === 'header'}
+        />
       </Link>
-    </div>
+    </LogoContainer>
   );
 };
 
